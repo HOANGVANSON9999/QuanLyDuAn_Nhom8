79c8dcb164339193f93a60cbb3ba6f66dc3edde9
son <vson99990@gmail.com>
2023-05-31 14:43:22 +0700

    //manhinhchao

---
+++ app/src/main/java/com/example/quanlyduan_nhom8/Manhinhchao.java
@@ -0,0 +1,33 @@
+package com.example.quanlyduan_nhom8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Manhinhchao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhchao);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
